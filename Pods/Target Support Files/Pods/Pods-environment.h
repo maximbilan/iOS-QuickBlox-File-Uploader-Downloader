26f5ce90b24a91452364f2ba09172c5dffec3269
Maxim Bilan <maximb.mail@gmail.com>
2015-04-13 20:58:31 +0300

    Added pods

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// QuickBlox
+#define COCOAPODS_POD_AVAILABLE_QuickBlox
+#define COCOAPODS_VERSION_MAJOR_QuickBlox 2
+#define COCOAPODS_VERSION_MINOR_QuickBlox 2
+#define COCOAPODS_VERSION_PATCH_QuickBlox 3
+
+// STHTTPRequest
+#define COCOAPODS_POD_AVAILABLE_STHTTPRequest
+#define COCOAPODS_VERSION_MAJOR_STHTTPRequest 1
+#define COCOAPODS_VERSION_MINOR_STHTTPRequest 0
+#define COCOAPODS_VERSION_PATCH_STHTTPRequest 2
+
